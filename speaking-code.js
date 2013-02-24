757959062d2511a115fc8cd1de9731926666235e
Thorsten Lorenz <thlorenz@gmx.de>
2013-02-24 18:39:47 -0500

    using deep equal and cleaning up eval-chunks

--- speaking-code.js
+++ speaking-code.js
@@ -20,7 +20,7 @@ function keyValuesToString(keyValues) {
     .join(' ');
 }
 
-var script = fs.readFileSync(path.join(__dirname, 'examples', 'objects-simple.js'), 'utf-8');
+var script = fs.readFileSync(path.join(__dirname, 'examples', 'changing-var.js'), 'utf-8');
 
 var chunked = chunkify(script);
 
@@ -29,7 +29,7 @@ evalChunks(chunked.chunks);
 function resolveTale(evaluated) {
   if (!evaluated) return '';
 
-  var taleStart =  '\n  '
+  var taleStart =  '\n'
     , tale = '';
 
   function addToTale(s) {
@@ -72,37 +72,3 @@ function resolveTales(chunks) {
 
 var tales = resolveTales(chunked.chunks);
 console.log(tales.map(function (x) { return x.tale; }).join('') );
-
-function enhanceCode () {
-var currentLine = 1;
-var enhancedCode = Object
-  .keys(chunked.chunks)
-  .map(function (k) {
-    var taleStart =  '\n\t'
-      , tale      =  taleStart 
-      , chunk        =  chunked.chunks[k]
-      , res          =  chunk.evaluated
-      , nextLine     =  chunk.end
-      , code         =  chunked.lines.slice(currentLine - 1, nextLine).join('\n');
-
-    function addToTale(s) {
-      tale += (taleStart + s);
-    }
-
-    currentLine = nextLine + 1;
-
-    if (!res) return code;
-
-    if (res.added.length) 
-      addToTale('+  ' + keyValuesToString(res.added));
-
-    if (res.changed.length) 
-      addToTale('~  ' + keyValuesToString(res.changed));
-
-    if (res.result) 
-      addToTale('=> ' + valueToString(res.result));
-
-    return code +  tale;
-  })
-  .join('\n');
-}
