26567ca8e19cafce9f5eaccb91a6338b4a12105f
Thorsten Lorenz <thlorenz@gmx.de>
2013-02-24 20:44:58 -0500

    extracting resolve tales module

--- speaking-code.js
+++ speaking-code.js
@@ -1,74 +1,17 @@
 'use strict';
 var fs         =  require('fs')
   , path       =  require('path')
-  , format     =  require('util').format
   , highlight  =  require('cardinal').highlight
-  , diffValues =  require('./lib/diff-values')
   , chunkify   =  require('./lib/chunkify')
   , evalChunks =  require('./lib/eval-chunks')
+  , resolveTales = require('./lib/resolve-tales')
   ;
-
-function valueToString(value) {
-  return typeof value !== 'object' ? value : JSON.stringify(value);
-}
-
-function keyValuesToString(keyValues) {
-  return keyValues
-    .map(function (x) { 
-      return format('[ %s = %s ]', x.key, valueToString(x.value));
-    })
-    .join(' ');
-}
 
 var script = fs.readFileSync(path.join(__dirname, 'examples', 'changing-var.js'), 'utf-8');
 
 var chunked = chunkify(script);
 
 evalChunks(chunked.chunks);
-
-function resolveTale(evaluated) {
-  if (!evaluated) return '';
-
-  var taleEnd =  '\n'
-    , tale = '';
-
-  function addToTale(s) {
-    tale += (s + taleEnd);
-  }
-
-  if (evaluated.added.length) {
-    evaluated.added
-      .forEach(function (x) {
-        var addTale = format('+ %s: %s', x.key, diffValues({}, x.value));
-        addToTale(addTale);
-      });
-  }
-
-  if (evaluated.changed.length) {
-    evaluated.changed
-      .forEach(function (x) {
-        var changeTale = format('~ %s: %s', x.key, diffValues(x.prevValue, x.value));
-        addToTale(changeTale);
-      });
-  }
-
-  if (evaluated.result) 
-    addToTale('=> ' + diffValues({}, evaluated.result));
-
-  return tale;
-}
-
-function resolveTales(chunks) {
-  return chunks
-    .map(function (chunk) {
-      var tale = resolveTale(chunk.evaluated);
-      return {
-          tale: tale
-        , insertAfter: chunk.end
-      };
-
-    });
-}
 
 var tales = resolveTales(chunked.chunks);
 
@@ -84,4 +27,3 @@ tales
 highlightedLines = highlightedLines.filter(function (x) { return x.length; });
 
 console.log(highlightedLines.join('\n'));
-
