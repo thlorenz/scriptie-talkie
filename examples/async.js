7a6b290a2d28378c82a587644c8e8489fb40fd79
Thorsten Lorenz <thlorenz@gmx.de>
2013-02-27 08:48:02 -0500

    using global as sandbox, cleaning all problematic props from it to avoid stack overflow in deepequal

--- examples/async.js
+++ examples/async.js
@@ -1,10 +1,24 @@
 'use strict';
 var fs = require('fs')
   , err
-  , files;
+  , files
+  , timeoutRes;
+
+setTimeout(function () {
+  timeoutRes = 'timed out';
+  console.log(timeoutRes);
+  secondTimeout();
+}, 500);
 
 fs.readdir(__dirname, function (err_, entries) {
   err = err_;
   files = entries;
   console.log('err: %s, files %s', err, files);
 });
+
+function secondTimeout() {
+  setTimeout(function () {
+    timeoutRes = 'timed out again';
+    console.log(timeoutRes);
+  }, 500);
+}
