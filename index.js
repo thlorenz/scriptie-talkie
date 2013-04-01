a7c5fb7cf8a2f28bba29731da1e40969f2977c67
Thorsten Lorenz <thlorenz@gmx.de>
2013-04-01 07:59:20 -0400

    proper talk module

--- index.js
+++ index.js
@@ -7,24 +7,39 @@ var fs           =  require('fs')
   , resolveTales =  require('./lib/resolve-tales')
   ;
 
+function highlightedLines(script) {
+  return highlight(script, { linenos: true }).split('\n');
+}
+
+var talk = module.exports = function (script, opts) {
+  opts         =  opts         || {};
+  opts.out     =  opts.out     || process.stdout;
+  opts.toLines =  opts.toLines || highlightedLines;
+
+  var snippets = snippetify(script);
+
+  evalSnippets(snippets, scriptPath, function (ctx) {
+    var tales = resolveTales(snippets);
+
+    var highlightedLines = opts.toLines(script) 
+      , offset = 0;
+
+    tales
+      .forEach(function (x) {
+        highlightedLines.splice(x.insertAfter + offset, 0, x.tale);
+        offset++;
+      });
+
+    highlightedLines = highlightedLines.filter(function (x) { return x.length; });
+
+    console.log(highlightedLines.join('\n'));
+  });
+
+};
+
+if (module.parent) return;
+
 var scriptPath = process.argv[2] || path.join(__dirname, 'examples', 'error.js')
   , script = fs.readFileSync(scriptPath, 'utf-8');
 
-var snippets = snippetify(script);
-
-evalSnippets(snippets, scriptPath, function (ctx) {
-  var tales = resolveTales(snippets);
-
-  var highlightedLines = highlight(script, { linenos: true }).split('\n')
-    , offset = 0;
-
-  tales
-    .forEach(function (x) {
-      highlightedLines.splice(x.insertAfter + offset, 0, x.tale);
-      offset++;
-    });
-
-  highlightedLines = highlightedLines.filter(function (x) { return x.length; });
-
-  console.log(highlightedLines.join('\n'));
-});
+talk(script);
