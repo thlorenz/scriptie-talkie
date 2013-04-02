f6aa3a6432b96e18f3cf7cfb3852078c48c6e8f4
Thorsten Lorenz <thlorenz@gmx.de>
2013-04-01 22:04:02 -0400

    adding tests for object diffs and errors

--- test/error.js
+++ test/error.js
@@ -5,10 +5,10 @@ var test    =  require('tape')
   , through =  require('through')
   , talk    =  require('..')
 
-var scriptPath =  require.resolve('../examples/changing-var')
+var scriptPath =  require.resolve('../examples/error')
   , script     =  require('fs').readFileSync(scriptPath, 'utf-8')
 
-test('\n# changing var\n', function (t) {
+test('\n# error\n', function (t) {
   talk(
       script    
     , scriptPath
@@ -25,23 +25,17 @@ test('\n# changing var\n', function (t) {
       , [ 'var a = 3;',
           '\u001b[92m+\u001b[39m  a: \u001b[34m\u001b[1m3\u001b[0m',
           '',
-          'a = a + 1;',
-          '\u001b[94m~\u001b[39m  a: \u001b[34m\u001b[1m4\u001b[0m',
-          '\u001b[36m=>\u001b[39m \u001b[34m\u001b[1m4\u001b[0m',
-          '',
-          'a++;',
-          '\u001b[94m~\u001b[39m  a: \u001b[34m\u001b[1m5\u001b[0m',
-          '\u001b[36m=>\u001b[39m \u001b[34m\u001b[1m4\u001b[0m',
+          'a + b;',
+          '\u001b[92m+\u001b[39m  \u001b[31m!\u001b[39m: \u001b[34m\u001b[1m"ReferenceError: b is not defined"\u001b[0m',
           '',
           'var b = 2;',
           '\u001b[92m+\u001b[39m  b: \u001b[34m\u001b[1m2\u001b[0m',
           '',
-          'b = b + a;',
-          '\u001b[94m~\u001b[39m  b: \u001b[34m\u001b[1m7\u001b[0m',
-          '\u001b[36m=>\u001b[39m \u001b[34m\u001b[1m7\u001b[0m',
+          'console.log(b.hello());',
+          '\u001b[92m+\u001b[39m  \u001b[31m!\u001b[39m: \u001b[34m\u001b[1m"TypeError: Object 2 has no method \'hello\'"\u001b[0m',
           '' ]
-      , 'shows intermediate results'
+      , 'shows intermediate results and shows exceptions'
     )
     t.end()
   }
-})
+});
---
+++ test/objects-simple.js
@@ -0,0 +1,36 @@
+'use strict';
+/*jshint asi: true */
+
+var test    =  require('tape') 
+  , through =  require('through')
+  , talk    =  require('..')
+
+var scriptPath =  require.resolve('../examples/objects-simple')
+  , script     =  require('fs').readFileSync(scriptPath, 'utf-8')
+
+test('\n# objects simple\n', function (t) {
+  talk(
+      script    
+    , scriptPath
+    , { toLines : function (script) { return script.split('\n') }
+      , write   : write 
+      }
+  );
+
+  function write(data) {
+    console.log(data);
+
+    t.deepEqual(
+        data.split('\n')
+      , [ 'var o = { a: 1 };',
+          '\u001b[92m+\u001b[39m  o: {\u001b[32m\u001b[1m"a" : 1\u001b[0m\u001b[36m\u001b[1m \u001b[0m }',
+          '',
+          'o.a = 2;',
+          '\u001b[94m~\u001b[39m  o: {"a" : \u001b[34m\u001b[1m2\u001b[0m\u001b[36m\u001b[1m // != 1\u001b[0m }',
+          '\u001b[36m=>\u001b[39m \u001b[34m\u001b[1m2\u001b[0m',
+          '' ]
+      , 'shows diffs of object properties'
+    )
+    t.end()
+  }
+})
