9972735448bf398f6e1a41e687620cd9754faaf1
Thorsten Lorenz <thlorenz@gmx.de>
2013-04-02 07:27:19 -0400

    shebang

--- bin/scriptie-talkie.js
+++ bin/scriptie-talkie.js
@@ -1,8 +1,8 @@
-'use strict'
+#!/usr/bin/env node
 
 var talk       =  require('..')
   , fs         =  require('fs')
-  , scriptPath =  process.argv[2]
+  , scriptPath =  process.argv[2];
 
 if (!scriptPath) {
   console.log('Usage: scriptie-talkie path/to/script.js');
