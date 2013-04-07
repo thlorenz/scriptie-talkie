05e7d80e4456fa581cf8bd9dcde94fa70b4653f2
Thorsten Lorenz <thlorenz@gmx.de>
2013-04-07 18:25:29 -0400

    disabling function declaration hoisting in browser as that breaks when functions use variables expected to be in context

---
+++ test/fixtures/function-using-context-vars.js
@@ -0,0 +1,8 @@
+var bar = 'bar';
+var foo = bar;
+
+function getFooAndBar() {
+  return foo + bar;
+}
+
+getFooAndBar();
---
+++ test/function-using-context-vars.js
@@ -0,0 +1,15 @@
+'use strict';
+/*jshint asi: true */
+
+var test    =  require('tap').test
+  , through =  require('through')
+  , talk    =  require('..')
+
+var scriptPath =  require.resolve('./fixtures/function-using-context-vars')
+  , script     =  require('fs').readFileSync(scriptPath, 'utf-8')
+
+
+test('\n# function-using-context-vars\n', function (t) {
+  var lines = talk(script, scriptPath);
+  t.end()
+})
