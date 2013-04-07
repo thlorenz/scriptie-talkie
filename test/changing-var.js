2db900aae1dc76d8d315c0e8ff00283dc7b58e2d
Thorsten Lorenz <thlorenz@gmx.de>
2013-04-07 11:13:37 -0400

    fixing changing var test

--- test/changing-var.js
+++ test/changing-var.js
@@ -12,32 +12,38 @@ test('\n# changing var\n', function (t) {
   var lines = talk(
       script    
     , scriptPath
-    , { toLines : function (script) { return script.split('\n') } }
+    , { toLines : function (script) { return script.split('\n') } 
+      , write   : write 
+    }
+
   );
 
-  console.log(lines.join('\n'));
+  function write(data) {
+    console.log(data);
 
-  t.deepEqual(
-      lines
-    , [ 'var a = 3;',
-        '\u001b[92m+\u001b[39m  a: \u001b[34m\u001b[1m3\u001b[0m',
-        '',
-        'a = a + 1;',
-        '\u001b[94m~\u001b[39m  a: \u001b[34m\u001b[1m4\u001b[0m',
-        '\u001b[36m=>\u001b[39m \u001b[34m\u001b[1m4\u001b[0m',
-        '',
-        'a++;',
-        '\u001b[94m~\u001b[39m  a: \u001b[34m\u001b[1m5\u001b[0m',
-        '\u001b[36m=>\u001b[39m \u001b[34m\u001b[1m4\u001b[0m',
-        '',
-        'var b = 2;',
-        '\u001b[92m+\u001b[39m  b: \u001b[34m\u001b[1m2\u001b[0m',
-        '',
-        'b = b + a;',
-        '\u001b[94m~\u001b[39m  b: \u001b[34m\u001b[1m7\u001b[0m',
-        '\u001b[36m=>\u001b[39m \u001b[34m\u001b[1m7\u001b[0m',
-        '' ]
-    , 'shows intermediate results'
-  )
-  t.end()
+    t.deepEqual(
+        data.split('\n')
+      , [ 'var a = 3;',
+          '\u001b[92m+\u001b[39m  a: \u001b[34m\u001b[1m3\u001b[0m',
+          '',
+          'a = a + 1;',
+          '\u001b[94m~\u001b[39m  a: \u001b[34m\u001b[1m4\u001b[0m',
+          '\u001b[36m=>\u001b[39m \u001b[34m\u001b[1m4\u001b[0m',
+          '',
+          'a++;',
+          '\u001b[94m~\u001b[39m  a: \u001b[34m\u001b[1m5\u001b[0m',
+          '\u001b[36m=>\u001b[39m \u001b[34m\u001b[1m4\u001b[0m',
+          '',
+          'var b = 2;',
+          '\u001b[92m+\u001b[39m  b: \u001b[34m\u001b[1m2\u001b[0m',
+          '',
+          'b = b + a;',
+          '\u001b[94m~\u001b[39m  b: \u001b[34m\u001b[1m7\u001b[0m',
+          '\u001b[36m=>\u001b[39m \u001b[34m\u001b[1m7\u001b[0m',
+          '',
+        ]
+      , 'shows intermediate results'
+    )
+    t.end()
+  }
 })
