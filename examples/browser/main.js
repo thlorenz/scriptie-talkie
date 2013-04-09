bafd7c1900dd75803bc024472f64ad91c5d195af
Thorsten Lorenz <thlorenz@gmx.de>
2013-04-08 22:46:29 -0400

    warning about incompatible browsers

--- examples/browser/main.js
+++ examples/browser/main.js
@@ -4,25 +4,12 @@
 
 var scriptieTalkie =  require('../../')
   , debounce       =  require('debounce')
+  , ua             =  require('useragent-wtf')
   , query          =  require('./query')
   , codeLink       =  document.getElementById('code-link')
   , codeTweet      =  document.getElementById('code-tweet')
   , root           =  getRoot()
   ;
-
-var term = require('hypernal')(104, 80);
-term.appendTo('#terminal');
-
-var editor = ace.edit("editor");
-editor.setTheme("ace/theme/monokai");
-editor.getSession().setMode("ace/mode/javascript");
-editor.on('change', debounce(evaluateScript, 400, false));
-editor.$highlightActiveLine = false;
-
-window.editor = editor;
-evaluateScript();
-
-initScript();
 
 function evaluateScript() {
   var script = editor.getValue();
@@ -56,8 +43,29 @@ function updateLinkAndTweet(code) {
 function initScript() {
   var parsed = query.parse();
   var code = (parsed && parsed.code) || require('./default-sample');
+
   if (code.charAt(code.length - 1) === '/') code = code.substring(0, code.length - 1);
 
   editor.setValue(code);
   updateLinkAndTweet(code);
+}
+
+if (ua.browser !== 'chrome' || ua.os === 'ipad') {
+  alert('scriptie-talkie is very sorry, but it will freeze any browser except chrome running on a pc or android and that would be evil wouldn\'t it?\n');
+} else {
+
+  var term = require('hypernal')(104, 80);
+  term.appendTo('#terminal');
+
+  var editor = ace.edit("editor");
+  editor.setTheme("ace/theme/monokai");
+  editor.getSession().setMode("ace/mode/javascript");
+  editor.on('change', debounce(evaluateScript, 400, false));
+  editor.$highlightActiveLine = false;
+
+  window.editor = editor;
+
+  initScript();
+  evaluateScript();
+
 }
