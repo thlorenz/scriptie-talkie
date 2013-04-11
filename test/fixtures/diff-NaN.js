968e94e490ea3317d7d66bed7bfeef753cc02d06
Thorsten Lorenz <thlorenz@gmx.de>
2013-04-11 07:16:38 -0400

    fixing NaN diffs by using deep-is instead of deep-equal

---
+++ test/fixtures/diff-NaN.js
@@ -0,0 +1,14 @@
+var a = 1 + void + 1;
+typeof a === 'undefined'
+
+var b = 1 + undefined + 1;
+typeof b === 'undefined'
+
+x = NaN + 1
+typeof x
+
+typeof NaN
+
+typeof (NaN + "")
+
+typeof (1 + void + 1)
