f57856caa90698c707f5c32165647bc2be7b092d
Thorsten Lorenz <thlorenz@gmx.de>
2013-04-11 07:53:14 -0400

    dev server to speed up browser testing

--- examples/browser/build.js
+++ examples/browser/build.js
@@ -1,7 +1,11 @@
 var browserify = require('browserify')
   , fs = require('fs');
 
-browserify()
-  .require(require.resolve('./main'), { entry: true })
-  .bundle({ debug: true })
-  .pipe(fs.createWriteStream(__dirname + '/bundle.js'));
+var bundle = module.exports = function () {
+  return browserify()
+    .require(require.resolve('./main'), { entry: true })
+    .bundle({ debug: true });
+};
+
+if (!module.parent)
+  bundle().pipe(fs.createWriteStream(__dirname + '/bundle.js'));
