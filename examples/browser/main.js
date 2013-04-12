848a0219ac7fd23cf119dbc14ca9a72314425857
Thorsten Lorenz <thlorenz@gmx.de>
2013-04-11 22:37:05 -0400

    adding meaningful error message for iPad safari failing

--- examples/browser/main.js
+++ examples/browser/main.js
@@ -22,7 +22,12 @@ function evaluateScript() {
   } catch (e) {
     console.log(e.stack);
     console.error(e.toString());
-    if (e.inner) {
+    if (e instanceof ReferenceError && /Trying to access object from destroyed plug-in/.test(e.message)) {
+      var msg = 'Looks like your iPad Safari browser doesn\'t like what scriptie talkie is doing.\n'
+              + 'Please try another device and/or browser, i.e. chrome works everywhere, even on iPad.';
+      term.writeln(msg);
+    }
+    else if (e.inner) {
       term.writeln('unable to parse the current code, looks like you have an error on: ');
       term.writeln('line: ' + e.inner.lineNumber + ' column: ' + e.inner.column);
     } else {
