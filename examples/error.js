d6c1e08f136455ca9ec22995ec70e548a9301362
Thorsten Lorenz <thlorenz@gmx.de>
2013-04-18 22:29:20 -0400

    properly removing error indicator from tracked values in browser
    - brings failed tests in browser down to 2

--- examples/error.js
+++ examples/error.js
@@ -1,7 +1,5 @@
 var a = 3;
-
 a + b;
 
 var b = 2;
-
 console.log(b.hello());
--- test/error.js
+++ test/error.js
@@ -16,17 +16,15 @@ test('\n# error\n', function (t) {
 
   t.deepEqual(
       lines 
-    , [ '\u001b[90m1: \u001b[39m\u001b[32mvar\u001b[39m \u001b[37ma\u001b[39m \u001b[93m=\u001b[39m \u001b[34m3\u001b[39m\u001b[90m;\u001b[39m',
-        '\u001b[92m+\u001b[39m  a: \u001b[34m\u001b[1m3\u001b[0m\n',
-        '\u001b[90m2: \u001b[39m',
-        '\u001b[90m3: \u001b[39m\u001b[37ma\u001b[39m \u001b[93m+\u001b[39m \u001b[37mb\u001b[39m\u001b[90m;\u001b[39m',
-        '\u001b[92m+\u001b[39m  \u001b[31m!\u001b[39m: \u001b[34m\u001b[1m"ReferenceError: b is not defined"\u001b[0m\n',
-        '\u001b[90m4: \u001b[39m',
-        '\u001b[90m5: \u001b[39m\u001b[32mvar\u001b[39m \u001b[37mb\u001b[39m \u001b[93m=\u001b[39m \u001b[34m2\u001b[39m\u001b[90m;\u001b[39m',
-        '\u001b[92m+\u001b[39m  b: \u001b[34m\u001b[1m2\u001b[0m\n',
-        '\u001b[90m6: \u001b[39m',
-        '\u001b[90m7: \u001b[39m\u001b[34mconsole\u001b[39m\u001b[32m.\u001b[39m\u001b[34mlog\u001b[39m\u001b[90m(\u001b[39m\u001b[37mb\u001b[39m\u001b[32m.\u001b[39m\u001b[37mhello\u001b[39m\u001b[90m(\u001b[39m\u001b[90m)\u001b[39m\u001b[90m)\u001b[39m\u001b[90m;\u001b[39m',
-        '\u001b[92m+\u001b[39m  \u001b[31m!\u001b[39m: \u001b[34m\u001b[1m"TypeError: Object 2 has no method \'hello\'"\u001b[0m\n' ]
+    , [ "\u001b[90m1: \u001b[39m\u001b[32mvar\u001b[39m \u001b[37ma\u001b[39m \u001b[93m=\u001b[39m \u001b[34m3\u001b[39m\u001b[90m;\u001b[39m",
+        "\u001b[92m+\u001b[39m  a: \u001b[34m\u001b[1m3\u001b[0m\n",
+        "\u001b[90m2: \u001b[39m\u001b[37ma\u001b[39m \u001b[93m+\u001b[39m \u001b[37mb\u001b[39m\u001b[90m;\u001b[39m",
+        "\u001b[92m+\u001b[39m  \u001b[31m!\u001b[39m: \u001b[34m\u001b[1m\"ReferenceError: b is not defined\"\u001b[0m\n",
+        "\u001b[90m3: \u001b[39m",
+        "\u001b[90m4: \u001b[39m\u001b[32mvar\u001b[39m \u001b[37mb\u001b[39m \u001b[93m=\u001b[39m \u001b[34m2\u001b[39m\u001b[90m;\u001b[39m",
+        "\u001b[92m+\u001b[39m  b: \u001b[34m\u001b[1m2\u001b[0m\n",
+        "\u001b[90m5: \u001b[39m\u001b[34mconsole\u001b[39m\u001b[32m.\u001b[39m\u001b[34mlog\u001b[39m\u001b[90m(\u001b[39m\u001b[37mb\u001b[39m\u001b[32m.\u001b[39m\u001b[37mhello\u001b[39m\u001b[90m(\u001b[39m\u001b[90m)\u001b[39m\u001b[90m)\u001b[39m\u001b[90m;\u001b[39m",
+        "\u001b[92m+\u001b[39m  \u001b[31m!\u001b[39m: \u001b[34m\u001b[1m\"TypeError: Object 2 has no method 'hello'\"\u001b[0m\n"]
     , 'shows intermediate results and shows exceptions'
   )
   t.end()
