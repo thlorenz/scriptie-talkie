b8504102ae14451017e239caf0405e213a852c55
Thorsten Lorenz <thlorenz@gmx.de>
2013-04-19 07:57:56 -0400

    removing hypernal fix in tests since hypernal itself is now fixed

--- test/browser/writeln.js
+++ test/browser/writeln.js
@@ -10,9 +10,5 @@ document.body.appendChild(terminal);
 
 var term = require('hypernal')(80, 80);
 term.appendTo(terminal);
-module.exports = function (s) { 
-  term.writeln(s);
-  // fix for the fact that hypernal doesn't interpret '\n' correctly at this point
-  if (s.charAt(s.length - 1) === '\n') term.writeln(' ');
-};
+module.exports = term.writeln.bind(term);
 }();
