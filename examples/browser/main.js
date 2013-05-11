8d7b82efee8ec048933fa98050d586c8cae6bb67
Thorsten Lorenz <thlorenz@gmx.de>
2013-05-11 15:33:48 -0400

    tabsize 2

--- examples/browser/main.js
+++ examples/browser/main.js
@@ -87,6 +87,7 @@ var editor = ace.edit("editor")
 editor.setTheme("ace/theme/monokai");
 session.setMode("ace/mode/javascript");
 editor.$highlightActiveLine = false;
+session.$tabSize = 2;
 
 initScript();
 
